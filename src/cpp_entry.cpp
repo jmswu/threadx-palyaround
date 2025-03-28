48b0581a2de506bab116e2886bffa9ad9d5502a0
James Wu <j.wu@mstglobal.com>
2025-03-28 20:04:15 +1000

    Test exception

--- src/cpp_entry.cpp
+++ src/cpp_entry.cpp
@@ -2,6 +2,7 @@
 #include "cpp_entry.hpp"
 #include "test.hpp"
 #include "etl/string.h"
+#include "etl/vector.h"
 
 void cpp_entry()
 {
@@ -14,4 +15,15 @@ void cpp_entry()
 
     etl::string<32> msg{"Hello from etl"};
     std::printf("%s\n", msg.c_str());
+
+    etl::vector<int, 3> vec{};
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    for(const auto val: vec)
+    {
+        std::printf("%i\n", val);
+    }
+
 }
