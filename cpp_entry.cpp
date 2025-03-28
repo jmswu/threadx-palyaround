5047f91029d28d07d1b595f8804bf90bbd933e8b
James Wu <j.wu@mstglobal.com>
2025-03-28 19:50:11 +1000

    Add etl lib

--- cpp_entry.cpp
+++ cpp_entry.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include "cpp_entry.hpp"
 #include "test.hpp"
+#include "etl/string.h"
 
 void cpp_entry()
 {
@@ -10,4 +11,7 @@ void cpp_entry()
     Test t{42};
 
     t.say_hello();
+
+    etl::string<32> msg{"Hello from etl"};
+    std::printf("%s\n", msg.c_str());
 }
